6ee6c06ab3e85762fdd1723855f9e1bb5bd2a0c4
WashingtonCC <tusobamadre@gmail.com>
2022-11-30 08:12:11 -0300

    migrations error user api

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -2,7 +2,11 @@
 Serializers for the API view.
 """
 #https://www.django-rest-framework.org/api-guide/serializers/
-from django.contrib.auth import get_user_model
+from django.contrib.auth import (
+    get_user_model,
+    authenticate,
+)
+from django.utils.translation import gettext as _
 from rest_framework import serializers
 
 
@@ -23,3 +27,40 @@ class UserSerializer(serializers.ModelSerializer):
         # encrypting the password.
         ### Serializers are just Forms for REST API's... ###
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update and return user."""
+        # instance is the Model instance to be updated
+        password = validated_data.pop('password', None) # remove pass from dict
+        user = super().update(instance, validated_data)
+
+        if password: # changing password is optional
+            user.set_password(password) # password is stored unhashed by default
+            user.save()
+        
+        return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user auth token."""
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False, # allow space at the end of pass
+    )
+
+    def validate(self, attrs):
+        """Validate and authenticate the user."""
+        email = attrs.get('email')
+        password = attrs.get('password')
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password,
+        )
+        if not user:
+            msg = _("Unable to authenticate with provided credentials.")
+            raise serializers.ValidationError(msg, code='authorization')
+        
+        attrs['user'] = user
+        return attrs
--- app/user/urls.py
+++ app/user/urls.py
@@ -8,5 +8,7 @@ from user import views
 app_name = "user" # userd for reverse('user:create')
 
 urlpatterns = [
-    path('create/', views.CreateUserView.as_view(), name="create")
+    path('create/', views.CreateUserView.as_view(), name="create"),
+    path('token/', views.CreateTokenView.as_view(), name="token"),
+    path('me/', views.ManageUserView.as_view(), name="me")
 ]
